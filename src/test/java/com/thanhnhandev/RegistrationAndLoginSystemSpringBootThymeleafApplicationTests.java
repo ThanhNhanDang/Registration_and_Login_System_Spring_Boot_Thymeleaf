ef8bdf2cc375012f1f83257ae5dfb7ebfe611788
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-12-31 12:19:32 +0700

    first commit

---
+++ src/test/java/com/thanhnhandev/RegistrationAndLoginSystemSpringBootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhnhandev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginSystemSpringBootThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
